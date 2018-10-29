2d7e0e3be39ca8e46d77318ad6de1840e20a9d72
John Edwin Ibe <jibe@blastasia.com>
2018-10-29 14:17:15 +0800

    first commit

---
+++ XamarinActivity1/XamarinActivity1/XamarinActivity1/QuotesPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinActivity1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class QuotesPage : ContentPage
+	{
+        private int counter = 0;        // current index of quote in the list of quotes
+        private int numOfQuotes;        // total number of quotes in the list
+        private List<String> quoteList; // list of quotes
+
+        public QuotesPage ()
+		{
+			InitializeComponent ();
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                Padding = new Thickness(20, 40, 20, 20);
+            }
+            else
+            {
+                Padding = new Thickness(20, 20, 20, 20);
+            }
+
+            // set fontSizeSlider's properties, 8 - min value and 32 - max value
+            this.SetFontSizeSliderProperties(fontSizeSlider, 8, 90);
+
+            if (this.quoteList == null)
+            {
+                this.quoteList = this.LoadAllQuotes();
+                this.numOfQuotes = this.quoteList.Count;
+            }
+
+            if(this.numOfQuotes <= 1)
+            {
+                nextButton.IsVisible = false;
+                backButton.IsVisible = false;
+            }
+            else
+            {
+                backButton.IsVisible = false;
+                this.AddNextButtonClickedEventHandler();
+                this.AddBackButtonClickedEventHandler();
+            }
+
+            if(quoteList != null)
+            {
+                quoteLabel.Text = this.quoteList[this.counter];
+            }
+		}
+
+        // Return list of quotes. 
+        private List<String> LoadAllQuotes()
+        {
+            return new List<String>()
+            {
+                "If you stop striving for PERFECTION, \nYou might as well be DEAD.",
+                "If you want to be seen, STAND UP!\nIf you want to be heard, SPEAK UP!\n And if you want to be appreciated, SHUT UP!",
+                "The world is not a wish granting factory.",
+                "Change your life today. Don't gamble on the future, act now, without delay.",
+                "The greatest failure is the failure to try."
+            };
+        }
+
+        // Add event handler for next button when clicked. This button moves the display quotes based on the array of quotes.
+        private void AddNextButtonClickedEventHandler()
+        {
+            nextButton.Clicked += (sender, e) =>
+            {
+                this.counter = this.counter + 1;
+                quoteLabel.Text = this.quoteList[this.counter];
+                backButton.IsEnabled = true;
+                backButton.IsVisible = true;
+
+                if (this.counter == (this.numOfQuotes - 1))
+                {
+                    nextButton.IsEnabled = false;
+                    nextButton.IsVisible = false;
+                }
+
+                scrollView.ScrollToAsync(0, 0, false);
+            };
+        }
+
+        // Add event handler for next button when clicked. This button moves backward the display quotes based  on the array of quotes.
+        private void AddBackButtonClickedEventHandler()
+        {
+            backButton.Clicked += (sender, e) =>
+            {
+                this.counter = this.counter - 1;
+                quoteLabel.Text = this.quoteList[this.counter];
+                nextButton.IsEnabled = true;
+                nextButton.IsVisible = true;
+
+                if(this.counter == 0)
+                {
+                    backButton.IsEnabled = false;
+                    backButton.IsVisible = false;
+                }
+                scrollView.ScrollToAsync(0, 0, false);
+            };
+        }
+
+         // Set properties of slider.   
+        private void SetFontSizeSliderProperties(Slider slider, double min, double max)
+        {
+            slider.Minimum = 0;
+            slider.Maximum = max;
+            slider.Value = min;
+
+            slider.ValueChanged += (sender, e) =>
+            {
+                if (slider.Value < min)
+                {
+                    slider.Value = min;
+                }
+            };
+        }
+	}
+}
---
+++ XamarinActivity2/XamarinActivity2/XamarinActivity2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinActivity2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if(Device.RuntimePlatform == Device.iOS)
+            {
+                Padding = new Thickness(20, 40, 20, 20);
+            }
+            else
+            {
+                Padding = new Thickness(20, 20, 20, 20);
+            }
+
+            // Set red, green and blue slider's properties
+            SetColorSliderProperties(redSlider);
+            SetColorSliderProperties(greenSlider);
+            SetColorSliderProperties(blueSlider);
+
+            // Set initial background color of box of type BoxView
+            SetBoxBackgroundColor();
+        }
+
+        /*
+         * Set properties of a color's slider, and add an event handler 
+         * that changes the background color of the box view depends on the 
+         * combinations of the current values of the given sliders, when the
+         * Value property changes. 
+         */
+        private void SetColorSliderProperties(Slider slider)
+        {
+            slider.Minimum = 0;
+            slider.Maximum = 255;
+            slider.ValueChanged += (sender, e) => SetBoxBackgroundColor();
+        }
+
+        /*
+         * Set background color of object box of type BoxView based on the
+         * combinations of the values of the given sliders.
+         */
+        private void SetBoxBackgroundColor()
+        {
+            box.BackgroundColor = Color.FromRgb(
+                    Convert.ToInt32(redSlider.Value),
+                    Convert.ToInt32(greenSlider.Value), 
+                    Convert.ToInt32(blueSlider.Value)
+                );
+        }
+    }
+}
